28d91c9a7bbbdf233e5e83eb005c36dd5288902d
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-09-19 16:29:54 +0530

    [sanjeeva] : added data into BST

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BSTassignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            tree.InserData(56);
+            tree.InserData(30);
+            tree.InserData(70);
+
+            Console.WriteLine("pre-transversal : root-left-right ");
+            tree.Display(tree.ReturnRoot());
+            Console.WriteLine("post-transversal : left-right-root");
+            tree.Post(tree.ReturnRoot());
+            Console.WriteLine("in order : left-right-root");
+            tree.Inorder(tree.ReturnRoot());
+        }
+    }
+}
---
+++ Tree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BSTassignment
+{
+    public class Tree
+    {
+        public Node root;
+        public Tree()
+        {
+            root = null;
+        }
+        public Node ReturnRoot()
+        {
+            return root;
+        }
+        public void InserData(int id)
+        {
+            Node node = new Node(id);
+            if (root == null)
+            {
+                root = node;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (id < current.item)
+                    {
+                        current = current.leftc;
+                        if (current == null)
+                        {
+                            parent.leftc = node;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current = current.rightc;
+                        if (current == null)
+                        {
+                            parent.rightc = node;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Display(Node root)
+        {
+            if (root != null)
+            {
+                Console.WriteLine(root.item + " |");
+                Display(root.leftc);
+                Display(root.rightc);
+            }
+        }
+
+        public void Post(Node root)
+        {
+            if (root != null)
+            {
+                Post(root.leftc);
+                Post(root.rightc);
+                Console.WriteLine(root.item + " |");
+            }
+
+        }
+        public void Inorder(Node root)
+        {
+            if (root != null)
+            {
+                Inorder(root.leftc);
+                Console.WriteLine(root.item + " |");
+                Inorder(root.rightc);
+
+            }
+
+        }
+    }
+}
